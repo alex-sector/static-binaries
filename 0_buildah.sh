ef8c8381fdf94718b62763ce89c30680c8a4b652
Olivier Dembour <33929198+alex-sector@users.noreply.github.com>
2021-01-15 09:28:07 +0100

    2021.01 Update     Use an alpine container     Update binaries version     Add the script used to generate the release

--- 0_buildah.sh
+++ 0_buildah.sh
@@ -1,7 +1,7 @@
-container=$(buildah from centos:6)
-buildah run $container yum install -y git wget patch gcc gcc-c++ xz flex bison
+container=$(buildah from alpine:3)
+buildah run $container sh -c "apk add git wget patch gcc g++ xz flex bison make curl perl file automake autoconf"
 buildah copy $container ./scripts /scripts
 buildah copy $container ./arch.txt /
-buildah run $container sh /scripts/0_install_centosmusl.sh
+buildah run $container sh /scripts/0_install_centosmusl.sh | tee install_musl.log
 buildah run $container rm -rf /build
 buildah commit --squash $container musl_compiler
